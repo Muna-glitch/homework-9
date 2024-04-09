42b3f8bdeeca703e0be9d173dd8043d2d47cfe40
Muna-glitch <68678829+Muna-glitch@users.noreply.github.com>
2024-04-08 22:37:31 -0400

    Updated question-3-answers.sql

--- sql/question-3-answers.sql
+++ sql/question-3-answers.sql
@@ -1,16 +1,19 @@
 -- select by id 1:
-
+SELECT * FROM `posts` WHERE id=1; 
 
 -- select all posts where title = includes "title 2":
-
+SELECT * FROM `posts` WHERE title like '%title 2%'; 
 
 -- select all posts and order by the title column alphabetically:
-
+SELECT * FROM `posts` ORDER BY title ASC; 
 
 -- insert 3 new posts
-
+insert into posts (title, description) values ('test post title 3', 'test post description 3');
+insert into posts (title, description) values ('test post title 4', 'test post description 4');
+insert into posts (title, description) values ('test post title 5', 'test post description 5');
 
 -- update posts where id = 1, set the title to "new title"
-
+update `posts` set `title`='Updated Post' where id=1; 
 
 -- delete post where id = 2
+delete from `posts` where id=2; 
